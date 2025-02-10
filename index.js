869b644f464e5e19334b267a8f0b2885ba7b987d
Kumarvikrm <kumarvikram68715@gmail.com>
2025-02-10 09:54:45 +0530

    add animation on music image and add animation on nav icons

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let progressEl = document.getElementById("progress");
 let songEl = document.getElementById("song");
 let ctrlIconEl = document.getElementById("ctrlIcon");
+let songImgEl = document.querySelector(".songImg");
 
 songEl.onloadedmetadata = function () {
   progressEl.max = songEl.duration;
@@ -12,10 +13,12 @@ function playPause() {
     songEl.pause();
     ctrlIconEl.classList.remove("fa-pause");
     ctrlIconEl.classList.add("fa-play");
+    songImgEl.style.animation = "none";
   } else {
-    songEl.play();  // Corrected typo
+    songEl.play();
     ctrlIconEl.classList.remove("fa-play");
     ctrlIconEl.classList.add("fa-pause");
+    songImgEl.style.animation = "rotate360 2s linear infinite";
   }
 }
 
@@ -23,11 +26,25 @@ songEl.addEventListener('play', () => {
   setInterval(() => {
     progressEl.value = songEl.currentTime;
   }, 500);
+  songImgEl.style.animation = "rotate360 2s linear infinite";
 });
+
+songEl.addEventListener('pause', ()=>{
+  songImgEl.style.animation = "none";
+});
+
+songEl.addEventListener('ended', ()=>{
+  songImgEl.style.animation = "none";
+});
+
+
 
 progressEl.onchange = function () {
   songEl.currentTime = progressEl.value;
   songEl.play();
   ctrlIconEl.classList.remove("fa-play");
   ctrlIconEl.classList.add("fa-pause");
+  songImgEl.style.animation = "rotate360 2s linear infinite";
 };
+
+
